9bf8eacdd05a1cdabf82d670734e2de40bb98e0b
Kevin-Antonio-Carvajal <kevin-antonio@ciencias.unam.mx>
2024-02-20 19:42:07 -0600

    Probando

---
+++ ejercicio-3-intro-to-c-connect-4-Kevin-Antonio-Carvajal-main/src/connect4.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "connect4.h"
+
+void 
+connect4_init (connect4* game, int rows, int columns, int num_of_players, int win_size) 
+{  
+}
+
+void
+connect4_free (connect4* game)
+{
+}
+
+char* 
+connect4_to_string (connect4* game)
+{
+  return "";
+}
+
+int 
+connect4_make_play (connect4* game, int player, int column)
+{
+  return 0;
+}
+
+bool 
+connect4_player_won (connect4* game, int player, int row, int column) 
+{
+  return false;
+}
